f9ecc43822aa660baaa650eeaf2c8957f684df9e
LNT175 <149796364+LNT175@users.noreply.github.com>
2023-11-10 20:11:43 +0300

    Add files via upload

---
+++ ДЗ-5 Уварова Романа 2035-ПБ-2÷3-213.py
@@ -0,0 +1,17 @@
+#ДЗ-5 Уварова Романа 2035-ПБ-2/3-213
+
+#Задание №1
+a=str(input())
+print(a[::-1])
+
+#Задание №2
+b=str(input())
+print(b.count("a")+b.count("e")+b.count("i")+b.count("o")+b.count("u"))
+
+#Задание №3
+c=str(input())
+print(c==c[::-1])
+
+#Задание №4
+d=str(input())
+print(d.count(" ")+1)
